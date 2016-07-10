e63df6710074e4bb41dafffe6bb1aa5b84932be7
John Doe <webdevnorth@gmail.com>
2016-07-10 19:44:49 +0100

    created main/routes components, placed within router (routes.js), mounted within index.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,3 +3,16 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+var routes = (
+	<Router history={hashHistory}>
+		<Route path='/' component={Main}>
+			<IndexRoute component={Home} />
+		</Route>
+	</Router>
+);
+
+module.exports = routes;

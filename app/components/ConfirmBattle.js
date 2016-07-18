713d73ec40dac21e2abe54f6455769fdc8c5578f
John Doe <webdevnorth@gmail.com>
2016-07-18 03:06:26 +0100

    refactored code to use MainContainer.js

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,11 +4,12 @@ var PropTypes = React.PropTypes;
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
 	return props.isLoading === true
 		? <p> LOADING! </p>
-		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+		: <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
@@ -28,7 +29,7 @@ function ConfirmBattle (props) {
             </Link>
           </div>
         </div>
-			</div>
+			</MainContainer>
 }
 
 ConfirmBattle.propTypes = {
